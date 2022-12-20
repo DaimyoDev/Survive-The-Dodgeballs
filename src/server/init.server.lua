d2d9b995eaf32c725d1e11bffa960387f2bbdff5
Raymond Brown <daimyoplays@gmail.com>
2022-12-19 22:51:37 -0600

    map randomizer

--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,10 +1,31 @@
 local intermissionTimer = game.ServerStorage["Intermission Timer"]
+local roundTimer = game.ServerStorage["Round Timer"]
 local roundSystem = require(script.roundsystem)
+local mapSelected = false
+local teleportPlayers = false
 
 while true do
     wait(1)
-    roundSystem.intermission(intermissionTimer)
+    if intermissionTimer.Value >= 0 then
+        roundSystem.intermission(intermissionTimer)
+    end
 
-    -- call intermission timer function
+    if intermissionTimer.Value <= 5 and intermissionTimer.Value >= 0 and not mapSelected then
+        roundSystem.selectRandomMap()
+        mapSelected = true
+    end
+
+    if intermissionTimer.Value <= 0 and not teleportPlayers then
+        roundSystem.teleportAndGetPlayers()
+        teleportPlayers = true
+    end
+    
+    if intermissionTimer.Value <= 0 and roundTimer.Value >= 0 then
+        roundSystem.round(roundTimer)
+    end
+    
+
+
+    --call function when round starts. Teleports players to randomly chosen map and starts the round timer plus the dodgeball spawner.
     
 end
--- src/server/roundsystem.lua
+++ src/server/roundsystem.lua
@@ -1,11 +1,45 @@
-local changeText = game.ReplicatedStorage.ChangeText 
-
+local changeText = game.ReplicatedStorage.ChangeText
 local roundSystem = {}
+local playersTeleportedToMap = false
+local playersTeleportedToLobby = false
 
 function roundSystem.intermission(intermissionTime)
 
     intermissionTime.Value -= 1
     changeText:FireAllClients(intermissionTime.Value, "Intermission")
 end
+
+function roundSystem.round(roundTime)
+
+    local playerList = game:GetService("Players"):GetPlayers()
+    for i, player in pairs(playerList) do
+        
+    end
+
+    roundTime.Value -= 1
+    changeText:FireAllClients(roundTime.Value, "Time Left")
+
+end
+
+function roundSystem.selectRandomMap()
+    local map1, map2, map3 = game.ServerStorage["Map 1"], game.ServerStorage["Map 2"], game.ServerStorage["Map 3"]
+    local mapList = {map1, map2, map3}
+    local selectedMapIndex = math.random(1, 3)
+
+    local selectedMap = mapList[selectedMapIndex]:Clone()
+    selectedMap.Parent = game.Workspace
+    playerSpawn = selectedMap["Player Spawn"]
+end
+
+
+function roundSystem.teleportAndGetPlayers()
+    local playerList = game:GetService("Players"):GetPlayers()
+    for i, player in pairs(playerList) do
+        local playerModel = player.Character.Humanoid.RootPart
+        print(playerModel.Position)
+        playerModel.Position = Vector3.new(playerSpawn.Position.x, playerSpawn.Position.y, playerSpawn.Position.z)
+    end
+end
+
 
 return roundSystem
